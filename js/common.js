c5118f9cd9fcac5113ebdc787ac79f5079d80c87
zxcgloomed <npaskov32@gmail.com>
2022-03-31 22:23:08 +0300

    fix png->jpg

--- js/common.js
+++ js/common.js
@@ -106,7 +106,7 @@ $(function(){
 				setTitle();
 				maker.addH2("", "", 'Добро пожаловать в just-crypt!');
 				maker.addP("", "", 'Please select cipher from the list on the left.');
-				maker.addHtml('<img src="./img/bg.png" style="width: 470px; height: 369px;">');
+				maker.addHtml('<img src="./img/bg.jpg" style="width: 470px; height: 369px;">');
 				maker.addP("", "", 'This website optimized for google chrome.');
 				maker.addP("", "", 'If you want, you can contact me by: <a href="mailto:plzdonotsay@gmail.com">plzdonotsay@gmail.com</a>');
 				break;
